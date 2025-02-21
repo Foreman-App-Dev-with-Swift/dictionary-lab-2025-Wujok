789718cd8cd0b991e8301ff4147cdec8cd421efe
Xavier Samuels <xsamuels@cps.edu>
2025-02-21 11:32:25 -0600

    jhgfd

--- Dictionary Lab/main.swift
+++ Dictionary Lab/main.swift
@@ -25,7 +25,9 @@ var practiceDictionary = ["rigmarole": "a lengthy and complicated procedure"]
  
 print("\n PROBLEM 1.0 : CREATE YOUR OWN \n")
 //Create a dictionary called mappingIntegers that that maps an integer (1) to its English word (one). And print it.
-
+var mappingIntegers : [Int : String] = [1 : "one"]
+print(mappingIntegers)
+print("The type of dictionary is [Int : String],the key is an integer and the value is a string. This dictionary is a shorthand type of dictary. I created this type of dictionary so it can map the Integer to its English word")
  
  
  
@@ -39,7 +41,10 @@ print("\n PROBLEM 2.0 : ADVANCED DICTIONARY \n")
 //Create a dictionary called favoriteWords that maps three words to its definition. Yes, you have to look up the definitions.
 //Then print favoriteWords.
 //Use the words cumulative, mnemonic, and satire. 
-
+var favoriteWords : [String : String] = [
+    "cumulative" : "increase of growing by accumulation or successive additions",
+    "mnemonic" : "something intended to assist the memory",
+    "satire" : "the use of irony, scarcasm, ridicule, or the like"]
  
  
  
